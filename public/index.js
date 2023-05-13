15bfbe9ffe23b9ba80a137a8e0cc6217b185026d
abodaa <abrahambogale747@gmail.com>
2023-05-13 13:56:59 +0300

    CSS file hooked

--- public/index.js
+++ public/index.js
@@ -6,24 +6,51 @@ const inpusubtitleDOM = document.querySelector(".input-subtitle");
 const inpuauthorDOM = document.querySelector(".input-author");
 const contentDOM = document.querySelector(".text-area");
 
+
 const formDOM = document.querySelector(".idea-form");
+
+// ************* All Ideas veiw ***************** //
 
 const showIdeas = async () => {
   try {
     const ideasData = await axios.get("/api/v1/ideas");
     const ideas = ideasData.data;
-    // console.log(ideas);
+    console.log(ideas);
     if (ideas.length < 1) {
-      ideasDOM.innerHTML = "<h4>There are no Ideas in the list</h4>";
+      return ideasDOM.innerHTML = "<h4>There are no Ideas in the list</h4>";
     }
     const allIdeas = ideas
       .map((idea) => {
         return `
-     <p class="author">By ${idea.name}</p>
-        <h2 class="title">${idea.title}</h2>
-    <p class="subtitle">${idea.subtitle}</p>
-     <p class="main-content">${idea.body}</p>
-     <div>---------------</div>
+
+        
+        <div class="container">
+          <div class="content-container">
+              <div class="user-name-pic">
+                  <img src="/user-pic.png" class="user-icon" alt="user">
+                  <p class="author">By ${idea.name}</p>
+              </div>
+              <h2 class="title">${idea.title}</h2>
+              <p class="subtitle">${idea.subtitle}</p>
+              <p class="main-content">${idea.body}</p>
+          </div>
+     
+          <div class="idea-delete-icons-container">
+            <!-- edit link -->
+            <a href="singleidea.html?id=${idea._id}"  class="edit-link">
+            <img src="/edit.png" class="edit-delete-icon" alt="user">
+
+            </a>
+            <!-- delete btn -->
+            <button type="button" class="delete-btn" data-id="${idea._id}">
+            <img src="/delete.png" class="edit-delete-icon" alt="user">
+
+            </button>
+          </div>
+          
+        </div>
+          
+      
     `;
       })
       .join("");
@@ -35,6 +62,9 @@ const showIdeas = async () => {
 };
 
 showIdeas();
+
+// ************* Create Ideas ***************** //
+
 
 formDOM.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -50,10 +80,30 @@ formDOM.addEventListener("submit", async (e) => {
       body: contentVal,
     });
 
-    alertDOM.innerHTML = `success!`;
+    alertDOM.innerHTML = `<p class="success-notification">Successfully shared. Thanks for sharing!</p>`;
+    setTimeout(() =>{
+      alertDOM.innerHTML = ""
+    },5000)
     showIdeas();
     // console.log(titleVal, subtitleVal, authorVal, contentVal);
   } catch (error) {
-    alertDOM.innerHTML = `error, please try again`;
+    alertDOM.innerHTML = `<p class="error-notification">Error, please try again</p>`;
+    setTimeout(() => {
+      alertDOM.innerHTML = "";
+    }, 5000);
   }
+});
+
+// ************* Delete Ideas ***************** //
+
+ideasDOM.addEventListener("click", async (e) => {
+    const el = e.target;
+    const id = el.parentElement.dataset.id;
+    try {
+      await axios.delete(`/api/v1/ideas/${id}`);
+      showIdeas();
+    } catch (error) {
+      console.log(error);
+    }
+ 
 });
